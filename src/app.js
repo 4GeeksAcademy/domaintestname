8930be2d05953f44d22d205fc12999dec29f22ea
Mauricio Reinales <146978569+Mortiz90@users.noreply.github.com>
2023-11-16 20:54:18 +0000

    test

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["our", "my", "your"];
+let adjective = ["black", "brown", "yellow", "red"];
+let noun = ["cat", "kangaroo", "cow", "turtle"];
+let extension = [".it", ".com", ".eu", ".fr", ".de", ".es"];
+let domainNamesArray = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adjective.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < extension.length; l++) {
+        domainNamesArray.push(
+          pronoun[i] + adjective[j] + noun[k] + extension[l]
+        );
+      }
+    }
+  }
+}
+document.getElementById("list-of-domains").innerHTML = domainNamesArray.join(
+  "<br>"
+);
